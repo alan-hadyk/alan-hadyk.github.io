94217905532e3f6e534203e5877f717b061444f8
Alan Hadyk <alan@omise.co>
2020-02-09 19:17:35 +0100

    Initial repo config

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import styled from "styled-components";
+
+const App = () => (
+	<App.Title>
+		Portfolio
+	</App.Title>
+);
+
+App.Title = styled.h1`
+	margin: 1rem;
+`;
+
+export default App;
