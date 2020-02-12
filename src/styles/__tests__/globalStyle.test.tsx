88a2a03e0b620980db24972906658d06efee2a92
Alan Hadyk <alan.hadyk@gmail.com>
2020-02-12 21:07:04 +0100

    [22] Fix fonts paths and add jest-styled-components (#4)
    * Update fonts paths
    
    * Add globalStyle.test and react testing library
    
    * Remove command from package.json

---
+++ src/styles/__tests__/globalStyle.test.tsx
@@ -0,0 +1,137 @@
+import React from "react";
+import { render } from "@testing-library/react";
+import { createGlobalStyle } from "styled-components";
+
+import globalStyle from "<styles>/globalStyle";
+
+describe("styles / globalStyle", () => {
+  describe("should have correct styles", () => {
+    const { sheet } = setup();
+    const { cssRules }: any = sheet;
+
+    test("html,body,div,span,applet,object,iframe,h1,h2,h3,h4,h5,h6,p,blockquote,pre,a,abbr,acronym,address,big,cite,code,del,dfn,em,img,ins,kbd,q,s,samp,small,strike,strong,sub,sup,tt,var,b,u,i,center,dl,dt,dd,ol,ul,li,fieldset,form,label,legend,table,caption,tbody,tfoot,thead,tr,th,td,article,aside,canvas,details,embed,figure,figcaption,footer,header,hgroup,menu,nav,output,ruby,section,summary,time,mark,audio,video,button,input", () => {
+  
+      const styles: CSSStyleDeclaration = findCSSStyles({
+        cssRules,
+        selectorText: "html,body,div,span,applet,object,iframe,h1,h2,h3,h4,h5,h6,p,blockquote,pre,a,abbr,acronym,address,big,cite,code,del,dfn,em,img,ins,kbd,q,s,samp,small,strike,strong,sub,sup,tt,var,b,u,i,center,dl,dt,dd,ol,ul,li,fieldset,form,label,legend,table,caption,tbody,tfoot,thead,tr,th,td,article,aside,canvas,details,embed,figure,figcaption,footer,header,hgroup,menu,nav,output,ruby,section,summary,time,mark,audio,video,button,input"
+      });
+  
+      expect(styles["margin"]).toEqual("0");
+      expect(styles["padding"]).toEqual("0");
+      expect(styles["border"]).toEqual("0");
+      expect(styles["font-family"]).toEqual("'Anonymous Pro',monospace");
+    });
+
+    test("article,aside,details,figcaption,figure,footer,header,hgroup,menu,nav,section", () => {
+      const styles: CSSStyleDeclaration = findCSSStyles({
+        cssRules,
+        selectorText: "article,aside,details,figcaption,figure,footer,header,hgroup,menu,nav,section"
+      });
+
+      expect(styles["display"]).toEqual("block");
+    });
+
+    test("html", () => {
+      const styles: CSSStyleDeclaration = findCSSStyles({
+        cssRules,
+        selectorText: "html"
+      });
+
+      expect(styles["font-size"]).toEqual("62.5%");
+    });
+
+    test("*,*:after,*:before,*::before,*::after", () => {
+      const styles: CSSStyleDeclaration = findCSSStyles({
+        cssRules,
+        selectorText: "*,*:after,*:before,*::before,*::after"
+      });
+
+      expect(styles["box-sizing"]).toEqual("border-box");
+    });
+
+    test("body", () => {
+      const styles: CSSStyleDeclaration = findCSSStyles({
+        cssRules,
+        selectorText: "body"
+      });
+
+      expect(styles["line-height"]).toEqual("1");
+      expect(styles["min-height"]).toEqual("100vh");
+      expect(styles["scroll-behavior"]).toEqual("smooth");
+      expect(styles["background-color"]).toEqual("#1e2224");
+      expect(styles["color"]).toEqual("#78b0b5");
+      expect(styles["position"]).toEqual("relative");
+    });
+    
+    test("h1,h2,h3,h4,h5,h6", () => {
+      const styles: CSSStyleDeclaration = findCSSStyles({
+        cssRules,
+        selectorText: "h1,h2,h3,h4,h5,h6"
+      });
+
+      expect(styles["font-family"]).toEqual("ExanModifiedRegular,monospace");
+    });
+
+    test("ul,ol", () => {
+      const styles: CSSStyleDeclaration = findCSSStyles({
+        cssRules,
+        selectorText: "ul,ol"
+      });
+
+      expect(styles["padding"]).toEqual("0");
+      expect(styles["margin"]).toEqual("0");
+      expect(styles["list-style"]).toEqual("none");
+    });
+
+    test("img", () => {
+      const styles: CSSStyleDeclaration = findCSSStyles({
+        cssRules,
+        selectorText: "img"
+      });
+
+      expect(styles["max-width"]).toEqual("100%");
+      expect(styles["display"]).toEqual("block");
+    });
+
+    test("a", () => {
+      const styles: CSSStyleDeclaration = findCSSStyles({
+        cssRules,
+        selectorText: "a"
+      });
+
+      expect(styles["text-decoration"]).toEqual("none");
+    });
+  });
+});
+
+function setup() {
+  const GlobalStyle = createGlobalStyle`
+    ${globalStyle}
+  `;
+
+  const utils = render(
+    <GlobalStyle />
+  );
+
+  return {
+    ...utils,
+    sheet: document.head.getElementsByTagName("style") && 
+      document.head.getElementsByTagName("style")[0] && 
+      document.head.getElementsByTagName("style")[0].sheet
+  };
+}
+
+interface findCSSStylesArgs {
+  cssRules: CSSStyleRule[];
+  selectorText: string;
+}
+
+function findCSSStyles({ 
+  cssRules = [],
+  selectorText
+}: findCSSStylesArgs): CSSStyleDeclaration  {
+  return cssRules
+    .find(cssRule => cssRule.selectorText === selectorText)
+    .style;
+}
+
