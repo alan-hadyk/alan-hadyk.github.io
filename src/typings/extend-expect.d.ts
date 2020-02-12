99daf9d00b5b85e2c379ca2c1ba4605df2fdc1e5
Alan Hadyk <alan@omise.co>
2020-02-12 21:02:12 +0100

    Add globalStyle.test and react testing library

---
+++ src/typings/extend-expect.d.ts
@@ -0,0 +1,7 @@
+// tslint:disable:no-namespace
+// eslint-disable-next-line no-unused-vars
+declare namespace jest {
+  interface Matchers<R> {
+    toHaveStyleRule: import("node_modules/jest-styled-components/typings/index.d.ts").jest.Matchers["toHaveStyleRule"]
+  }
+}
