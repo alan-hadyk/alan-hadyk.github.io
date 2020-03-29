2e03b846e5ef2599035c811111570c564237ff3f
Alan Hadyk <alan@omise.co>
2020-03-29 18:14:19 +0200

    Initial commit - FpsChart.Container

--- src/UI/molecules/FpsChart.tsx
+++ src/UI/molecules/FpsChart.tsx
@@ -1,15 +1,28 @@
-import React, { memo, Fragment } from "react";
+import React, { memo } from "react";
+import styled, { css, FlattenSimpleInterpolation } from "styled-components";
+// import { transparentize, radialGradient } from "polished";
 
 import PositionContainer from "<layout>/PositionContainer";
 
 // TODO - tests after component will be ready
 const FpsChart = (): JSX.Element => (
   <PositionContainer
+    position="relative"
   >
-    <Fragment>
+    <FpsChart.Container>
       FPS
-    </Fragment>
+    </FpsChart.Container>
   </PositionContainer>
 );
+
+FpsChart.Container = styled.div`
+  ${({
+    theme: {
+      colorPalette: { blue400 }
+    }
+  }): FlattenSimpleInterpolation => css`
+    border: thin solid ${blue400};
+  `}
+`;
   
 export default memo(FpsChart);
