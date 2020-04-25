ef78b81782ea3aac755a0054c584973f9bfc595b
Alan Hadyk <alan@omise.co>
2020-04-25 18:13:32 +0200

    Few adjustments

--- src/UI/atoms/Globe.tsx
+++ src/UI/atoms/Globe.tsx
@@ -1,14 +1,9 @@
 import React from "react";
-import styled, { css, FlattenSimpleInterpolation } from "styled-components";
 import ReactGlobe from "react-globe";
 
 import colorPalette from "<styles>/variables/colorPalette";
 
 const Globe = (): JSX.Element => (
-  // <Globe.Container
-  //   data-testid="Globe"
-  // >
-  // </Globe.Container>
   <ReactGlobe
     globeOptions={{
       enableBackground: false,
@@ -18,23 +13,10 @@ const Globe = (): JSX.Element => (
       glowColor: colorPalette.white,
       glowPower: 6,
       glowRadiusScale: 0.25,
-      texture: "/images/miniature-earth.svg"
+      texture: "/portfolio/Hologram.png"
+      // texture: "https://raw.githubusercontent.com/alan-hadyk/portfolio/master/src/assets/images/miniature-earth.svg"
     }}
   />
 );
-
-Globe.Container = styled.div`
-  ${({
-    theme: {
-      spacing
-    }
-  }): FlattenSimpleInterpolation => css`
-    bottom: 0;
-    left: 0;
-    position: absolute;
-    right: 0;
-    top: 0;
-  `}
-`;
   
 export default Globe;
