45029472de7a2936aca8967d87e01691c6e6ae1f
alan-hadyk <alan.hadyk@gmail.com>
2020-05-24 14:03:21 +0200

    Company.test.tsx - part 2

--- src/UI/organisms/__tests__/Company.test.tsx
+++ src/UI/organisms/__tests__/Company.test.tsx
@@ -116,95 +116,277 @@ describe("organisms / Company", () => {
       expect(Timeline.children[1]).toHaveStyleRule("width", ".2rem");
     });
   });
-  
-  describe.skip("IconsWithLabels", () => {
-    const iconsWithLabels: IconWithLabelProps[] = [
-      {
-        iconName: "brandWebpack",
-        label: "Webpack"
-      },
-      {
-        iconName: "node",
-        label: "Node"
-      }
-    ];
 
-    test("there should be correct number of icons", () => {
-      const { Icons } = setup({
-        iconsWithLabels
-      });
-
-      expect(Icons.length).toEqual(3);
-    });
-
-    test("icons should render correct SVGs passed via iconsWithLabels.iconName props", () => {
-      const { Icons } = setup({
-        iconsWithLabels
-      });
-
-      expect(Icons[1].textContent).toEqual("Brand-Webpack.svg");
-      expect(Icons[2].textContent).toEqual("Icon-Node.svg");
-    });
-
-    test("should have correct content passed via iconsWithLabels.label props", () => {
-      const { IconsWithLabels } = setup({
-        iconsWithLabels
-      });
-
-      expect(IconsWithLabels.children[0].children[1].textContent).toEqual("Webpack");
-      expect(IconsWithLabels.children[1].children[1].textContent).toEqual("Node");
-    });
-
+  describe("CompanyInnerFlexContainer", () => {
     describe("Props", () => {
-      describe("size - small", () => {
-        describe("FlexContainer - gap", () => {    
-          test("should have 1.6rem in all children, except the first one", () => {
-            const { IconsWithLabels } = setup();
-            
-            expect(IconsWithLabels).toHaveStyleRule("margin-left", "1.6rem", {
-              modifier: "& > *"
-            });
-            expect(IconsWithLabels).toHaveStyleRule("margin-left", "0", {
-              modifier: "& > *:first-child"
-            });
-          });
-        });
+      let CompanyInnerFlexContainer: Element;
 
-        describe("IconContainer - height", () => {
-          test("should have 2.8rem", () => {
-            const { IconsWithLabels } = setup();
-    
-            expect(IconsWithLabels.children[0].children[0].children[0]).toHaveStyleRule("height", "2.8rem");
-            expect(IconsWithLabels.children[1].children[0].children[0]).toHaveStyleRule("height", "2.8rem");
-          });
-        });
+      beforeEach(() => {
+        CompanyInnerFlexContainer = setup().CompanyInnerFlexContainer;
+      });
 
-        describe("IconWithLabelSpacingContainer - paddingRight", () => {
-          test("should have .8rem", () => {
-            const { IconsWithLabels } = setup();
-    
-            expect(IconsWithLabels.children[0].children[0]).toHaveStyleRule("padding-right", ".8rem");
-            expect(IconsWithLabels.children[0].children[0]).toHaveStyleRule("padding-right", ".8rem");
-          });
+      describe("alignItems", () => {
+        test("should have flex-start", () => {
+          expect(CompanyInnerFlexContainer).toHaveStyleRule("align-items", "flex-start");
         });
+      });
 
-        describe("LabelTexts - fontSize", () => {  
-          test("should have 16px", () => {
-            const { IconsWithLabels } = setup();
-  
-            expect(IconsWithLabels.children[0].children[1]).toHaveStyleRule("font-size", "16px");
-            expect(IconsWithLabels.children[1].children[1]).toHaveStyleRule("font-size", "16px");
+      describe("flexFlow", () => {
+        test("should have row nowrap", () => {
+          expect(CompanyInnerFlexContainer).toHaveStyleRule("flex-flow", "row nowrap");
+        });
+      });
+
+      describe("gap", () => {
+        test("should apply margin-left equal to 8.8rem to all children except the first one", () => {
+          expect(CompanyInnerFlexContainer).toHaveStyleRule("margin-left", "8.8rem", {
+            modifier: "& > *"
+          });
+          expect(CompanyInnerFlexContainer).toHaveStyleRule("margin-left", "0", {
+            modifier: "& > *:first-child"
           });
         });
       });
 
-      describe("position - horizontal", () => {
-        describe("FlexContainer - flexFlow", () => {      
-          test("should have row nowrap", () => {
-            const { IconsWithLabels } = setup();
-  
-            expect(IconsWithLabels).toHaveStyleRule("flex-flow", "row nowrap");
+      describe("maxWidth", () => {
+        test("should have 105.6rem", () => {
+          expect(CompanyInnerFlexContainer).toHaveStyleRule("max-width", "105.6rem");
+        });
+      });
+    });
+  });
+
+  describe("FlexItem", () => {
+    let FlexItem: Element[];
+
+    beforeEach(() => {
+      FlexItem = setup().FlexItem;
+    });
+
+    describe("FlexItem[0]", () => {
+      describe("Props", () => {
+        describe("flex", () => {
+          test("should have 0 0 50%", () => {
+            expect(FlexItem[0]).toHaveStyleRule("flex", "0 0 50%");
           });
+        });
+      });
+    });
+
+    describe("FlexItem[1]", () => {
+      describe("Props", () => {
+        describe("flex", () => {
+          test("should have 0 0 50%", () => {
+            expect(FlexItem[1]).toHaveStyleRule("flex", "0 0 50%");
+          });
+        });
+      });
+    });
+  });
+
+  describe("LogoFlexContainer", () => {
+    describe("Props", () => {
+      let LogoFlexContainer: Element;
+
+      beforeEach(() => {
+        LogoFlexContainer = setup().LogoFlexContainer;
+      });
+
+      describe("alignItems", () => {
+        test("should have flex-start", () => {
+          expect(LogoFlexContainer).toHaveStyleRule("align-items", "flex-start");
+        });
+      });
+
+      describe("justifyContent", () => {
+        test("should have flex-end", () => {
+          expect(LogoFlexContainer).toHaveStyleRule("justify-content", "flex-end");
+        });
+      });
+    });
+  });
+
+  describe("Logo", () => {
+    describe("Props", () => {
+      describe("height", () => {
+        test("should have 4.8rem", () => {
+          const { Logo } = setup();
+
+          expect(Logo).toHaveStyleRule("height", "4.8rem");
+        });
+      });
+
+      describe("iconName", () => {
+        test("should have Icon-Apollo.svg when prop logo: apollo", () => {
+          const { Logo } = setup({
+            logo: "apollo"
+          });
+
+          expect(Logo.textContent).toEqual("Icon-Apollo.svg");
+        });
+
+        test("should have Company-Omise.svg when prop logo: companyOmise", () => {
+          const { Logo } = setup({
+            logo: "companyOmise"
+          });
+
+          expect(Logo.textContent).toEqual("Company-Omise.svg");
+        });
+
+        test("should have Brand-JS.svg when prop logo: brandJS", () => {
+          const { Logo } = setup({
+            logo: "brandJS"
+          });
+
+          expect(Logo.textContent).toEqual("Brand-JS.svg");
+        });
+      });
+
+      describe("isHeightResponsive", () => {
+        describe("svg", () => {
+          describe("height", () => {
+            test("should have 100%", () => {
+              const { Logo } = setup();
+
+              expect(Logo).toHaveStyleRule("height", "100%", {
+                modifier: "svg"
+              });
+            });
+          });
+
+          describe("width", () => {
+            test("should have auto", () => {
+              const { Logo } = setup();
+
+              expect(Logo).toHaveStyleRule("width", "auto", {
+                modifier: "svg"
+              });
+            });
+          });
+        });
+      });
+    });
+  });
+
+  describe("CompanyDescription", () => {
+    describe("Props", () => {
+      describe("date", () => {
+        test("should render label equal to date prop - July 2016 to present", () => {
+          const { CompanyDescription } = setup({
+            date: "July 2016 to present"
+          });
+
+          expect(CompanyDescription.children[1].children[0].children[1].textContent).toEqual("July 2016 to present");
+        });
+
+        test("should render label equal to date prop - March 2017 to August 2018", () => {
+          const { CompanyDescription } = setup({
+            date: "March 2017 to August 2018"
+          });
+
+          expect(CompanyDescription.children[1].children[0].children[1].textContent).toEqual("March 2017 to August 2018");
+        });
+      });
+
+      describe("iconsWithLabels", () => {
+        test("should render correct icons - Webpack, Node", () => {
+          const { CompanyDescription } = setup({
+            iconsWithLabels: [
+              {
+                iconName: "brandWebpack",
+                label: "Webpack"
+              },
+              {
+                iconName: "node",
+                label: "Node"
+              }
+            ]
+          });
+          const IconsWithLabels: Element = CompanyDescription.children[2].children[1].children[0];
+
+          expect(IconsWithLabels.children.length).toEqual(2);
+          expect(IconsWithLabels.children[0].children[0].children[0].textContent).toEqual("Brand-Webpack.svg");
+          expect(IconsWithLabels.children[0].children[1].textContent).toEqual("Webpack");
+          expect(IconsWithLabels.children[1].children[0].children[0].textContent).toEqual("Icon-Node.svg");
+          expect(IconsWithLabels.children[1].children[1].textContent).toEqual("Node");
+        });
+
+        test("should render correct icons - React, Apollo", () => {
+          const { CompanyDescription } = setup({
+            iconsWithLabels: [
+              {
+                iconName: "brandReact",
+                label: "React"
+              },
+              {
+                iconName: "apollo",
+                label: "Apollo"
+              }
+            ]
+          });
+          const IconsWithLabels: Element = CompanyDescription.children[2].children[1].children[0];
+
+          expect(IconsWithLabels.children.length).toEqual(2);
+          expect(IconsWithLabels.children[0].children[0].children[0].textContent).toEqual("Brand-React.svg");
+          expect(IconsWithLabels.children[0].children[1].textContent).toEqual("React");
+          expect(IconsWithLabels.children[1].children[0].children[0].textContent).toEqual("Icon-Apollo.svg");
+          expect(IconsWithLabels.children[1].children[1].textContent).toEqual("Apollo");
+        });
+      });
+
+      describe("responsibilites", () => {
+        test("should render correct responsibilites - test 1", () => {
+          const responsibilites: CompanyProps["responsibilites"] = [
+            "Create scalable and sane front-end architecture", 
+            "Orchestrate and direct collaborative team operational objectives for front-end layer of an application designed to manage payment systems"    
+          ];
+          const { CompanyDescription } = setup({
+            responsibilites
+          });
+          const UnorderedList: Element = CompanyDescription.children[3].children[1].children[0];
+
+          expect(UnorderedList.children.length).toEqual(2);
+          responsibilites.forEach((responsibility: string, index: number) => {
+            expect(UnorderedList.children[index].textContent).toEqual(responsibility);
+          });
+        });
+
+        test("should render correct responsibilites - test 2", () => {
+          const responsibilites: CompanyProps["responsibilites"] = [
+            "Apply development and certify adherence to best practices to deliver PoC based on React.js", 
+            "Transform and create reusable components and manage adapters, models and serializers"    
+          ];
+          const { CompanyDescription } = setup({
+            responsibilites
+          });
+          const UnorderedList: Element = CompanyDescription.children[3].children[1].children[0];
+
+          expect(UnorderedList.children.length).toEqual(2);
+          responsibilites.forEach((responsibility: string, index: number) => {
+            expect(UnorderedList.children[index].textContent).toEqual(responsibility);
+          });
+        });
+      });
+
+      describe("title", () => {
+        test("should render correct title - test 1", () => {
+          const title: CompanyProps["title"] = "Front-end developer";
+          const { CompanyDescription } = setup({
+            title
+          });
+          const Title: string = CompanyDescription.children[0].textContent;
+
+          expect(Title).toEqual(title);
+        });
+
+        test("should render correct title - test 2", () => {
+          const title: CompanyProps["title"] = "Freelancer";
+          const { CompanyDescription } = setup({
+            title
+          });
+          const Title: string = CompanyDescription.children[0].textContent;
+
+          expect(Title).toEqual(title);
         });
       });
     });
