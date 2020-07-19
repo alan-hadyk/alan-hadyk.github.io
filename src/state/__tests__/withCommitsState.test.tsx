38e5fa48fa1aa07e09920b15b34521ab5968f98e
Alan Hadyk <alan.hadyk@gmail.com>
2020-07-19 17:35:34 +0200

    Update dependencies (#32)
    * Update dependencies
    
    * Fix eslint issues
    
    * Add jest-environment-jsdom-sixteen

--- src/state/__tests__/withCommitsState.test.tsx
+++ src/state/__tests__/withCommitsState.test.tsx
@@ -39,7 +39,6 @@ describe("state / withCommitsState", () => {
                   date: "2020-03-15T14:58:16Z"
                 }
               },
-              // eslint-disable-next-line @typescript-eslint/camelcase
               html_url: "https://github.com/alan-hadyk/portfolio/commit/6f05bb91f454878edcb0f0e30e39501d39b46e4f",
               sha: "6f05bb91f454878edcb0f0e30e39501d39b46e4f"
             },
@@ -49,7 +48,6 @@ describe("state / withCommitsState", () => {
                   date: "2020-03-14T16:05:26Z"
                 }
               },
-              // eslint-disable-next-line @typescript-eslint/camelcase
               html_url: "https://github.com/alan-hadyk/portfolio/commit/b18b6616d0da725d49decc1b1f63c3322ca9c3c5",
               sha: "b18b6616d0da725d49decc1b1f63c3322ca9c3c5"
             }
