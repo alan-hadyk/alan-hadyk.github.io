ae9603f7ea5b674f7f925cc2533b3fc4c32d8c37
alan-hadyk <alan.hadyk@gmail.com>
2020-08-25 14:01:40 +0200

    Fix withCommitsState

--- src/state/withCommitsState.tsx
+++ src/state/withCommitsState.tsx
@@ -1,4 +1,3 @@
-/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
 import React, { useState, useEffect } from "react";
 
 import fetch, { Response } from "node-fetch";
@@ -6,31 +5,44 @@ import fetch, { Response } from "node-fetch";
 import {
   CommitProps
 } from "<molecules>/__typings__/Commit.d.ts";
+import {
+  ComponentWithCommitsType
+} from "<state>/__typings__/withCommitsState.d.ts";
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-const withCommitsState = (WrappedComponent: React.FunctionComponent<any>) => (props: any): JSX.Element => {
-  const [commitsList, setCommitsList] = useState<[] | CommitProps[]>([]);
-  const [hasError, setError] = useState<boolean>(false);
+const withCommitsState = (WrappedComponent: React.FunctionComponent<any>): ComponentWithCommitsType => {
+  const ComponentWithCommits = (props: unknown): JSX.Element => {
+    const [commitsList, setCommitsList] = useState<[] | CommitProps[]>([]);
+    const [hasError, setError] = useState<boolean>(false);
 
-  useEffect(() => {
-    fetchCommits();
-  }, [hasError]);
+    useEffect(() => {
+      fetchCommits();
+    }, [hasError]);
 
-  return (
-    <WrappedComponent commitsList={commitsList} hasError={hasError} {...props} />
-  );
+    return (
+      <WrappedComponent commitsList={commitsList} hasError={hasError} {...props} />
+    );
 
-  async function fetchCommits(): Promise<void> {
-    try {
-      const data: Response = await fetch("https://api.github.com/repos/alan-hadyk/portfolio/commits");
-      const json: CommitProps[] = await data.json();
+    async function fetchCommits(): Promise<void> {
+      try {
+        const data: Response = await fetch("https://api.github.com/repos/alan-hadyk/portfolio/commits");
+        const json: CommitProps[] = await data.json();
 
-      setCommitsList(json);
-      setError(false);
-    } catch(err) {
-      setError(true);
+        if (!data.ok || !Array.isArray(json)) {
+          setCommitsList([]);
+          setError(true);
+        } else {
+          setCommitsList(json);
+          setError(false);
+        }
+      } catch(err) {
+        setCommitsList([]);
+        setError(true);
+      }
     }
-  }
+  };
+
+  return ComponentWithCommits;
 };
 
 export default withCommitsState;
