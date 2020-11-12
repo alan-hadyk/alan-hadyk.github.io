1690d3c99d102fb8c998bb08d97b34b047b92e80
alan-hadyk <alan.hadyk@gmail.com>
2020-11-12 17:48:37 +0100

    Update comment

--- src/index.tsx
+++ src/index.tsx
@@ -61,5 +61,7 @@ Promise.all(polyfills)
     console.error("Failed fetching polyfills", error);
   });
 
-// More about service workers: https://bit.ly/CRA-PWA
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
 serviceWorker.unregister();
