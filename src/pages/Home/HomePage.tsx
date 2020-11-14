d206a65fd7520685f8556d6f3c038bdd76d4f257
alan-hadyk <alan.hadyk@gmail.com>
2020-11-14 11:13:06 +0100

    Add Suspense to HomePage

--- src/pages/Home/HomePage.tsx
+++ src/pages/Home/HomePage.tsx
@@ -22,3 +22,50 @@ const HomePage = (): JSX.Element => (
 );
 
 export default memo(HomePage);
+
+// import React, { Suspense, memo, lazy } from "react";
+
+// const DashboardSection = lazy(
+//   () => import("pages/Home/sections/dashboard/DashboardSection")
+// );
+// const ExperienceSection = lazy(
+//   () => import("pages/Home/sections/experience/ExperienceSection")
+// );
+// const SkillsSection = lazy(
+//   () => import("pages/Home/sections/skills/SkillsSection")
+// );
+// const AboutMeSection = lazy(
+//   () => import("pages/Home/sections/about/AboutMeSection")
+// );
+// const ContactSection = lazy(
+//   () => import("pages/Home/sections/contact/ContactSection")
+// );
+
+// const Header = lazy(() => import("UI/organisms/Header"));
+// import PageTemplate from "UI/templates/PageTemplate";
+
+// const HomePage = (): JSX.Element => (
+//   <PageTemplate>
+
+//     <Suspense fallback={<div />}>
+//       <DashboardSection />
+//     </Suspense>
+//     <Suspense fallback={<div />}>
+//       <ExperienceSection />
+//     </Suspense>
+//     <Suspense fallback={<div />}>
+//       <SkillsSection />
+//     </Suspense>
+//     <Suspense fallback={<div />}>
+//       <AboutMeSection />
+//     </Suspense>
+//     <Suspense fallback={<div />}>
+//       <ContactSection />
+//     </Suspense>
+//     <Suspense fallback={<div />}>
+//       <Header zIndex="layer2" />
+//     </Suspense>
+//   </PageTemplate>
+// );
+
+// export default memo(HomePage);
