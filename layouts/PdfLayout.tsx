f4e8e9ce220661f2fd748292864c12e5539e6e2c
alan-hadyk <alan.hadyk@gmail.com>
2023-03-20 17:01:21 +0100

    Fix PdfLayout

--- layouts/PdfLayout.tsx
+++ layouts/PdfLayout.tsx
@@ -1,8 +1,14 @@
 import { PDFTemplate } from "components/templates/PDFTemplate";
 import { IPdfLayoutProps } from "layouts/@types/PdfLayout";
 import React from "react";
+import Head from "next/head";
 
-const PdfLayout: React.FC<IPdfLayoutProps> = ({ children }) =>
-  children ? <PDFTemplate>{children}</PDFTemplate> : null;
-
+const PdfLayout: React.FC<IPdfLayoutProps> = ({ children, title }) => (
+  <>
+    <Head>
+      <title>{title}</title>
+    </Head>
+    {children ? <PDFTemplate>{children}</PDFTemplate> : null}
+  </>
+);
 export { PdfLayout };
