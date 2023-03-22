a610f1bfc7348dbd38de82f4ff607104c4fbef2b
Alan Hadyk <alan.hadyk@gmail.com>
2023-03-22 12:14:57 +0100

    Refactor TechStack (#307)

---
+++ components/molecules/TechStack/styles.ts
@@ -0,0 +1,18 @@
+import { IThemeClasses } from "types/theme";
+
+export const techStackDefaultThemeClasses: IThemeClasses = {
+  marginBottom: "mb-16"
+};
+
+export const techStackTitleDefaultThemeClasses: IThemeClasses = {
+  color: "text-blue100",
+  fontFamily: "font-anonymousPro",
+  fontSize: "text-24",
+  fontWeight: "font-bold",
+  lineHeight: "leading-32"
+};
+
+export const techStackIconsWrapperDefaultThemeClasses: IThemeClasses = {
+  marginLeft: "ml-16",
+  marginTop: "mt-16"
+};
