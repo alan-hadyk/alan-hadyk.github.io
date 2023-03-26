1721e2e1a736c3a28df4fcb62886a62be8267ce9
alan-hadyk <alan.hadyk@gmail.com>
2023-03-26 17:29:06 +0200

    Update components/molecules/CompanyName/styles.ts

--- components/molecules/CompanyName/styles.ts
+++ components/molecules/CompanyName/styles.ts
@@ -1,3 +1,4 @@
+/* eslint-disable import/exports-last */
 import { isIE11 } from "helpers/browser/isIE11";
 import { IThemeClasses } from "types/theme";
 
@@ -15,18 +16,20 @@ export const companyTvDesktopAndTabletNameContainerDefaultThemeClasses: IThemeCl
       : []
   };
 
-export const companyTvDesktopAndTabletNameDefaultThemeClasses: IThemeClasses = {
+const companyNameCommonThemeClasses: IThemeClasses = {
   color: "text-white",
   fontFamily: isIE11() ? "font-anonymousPro" : "font-exan",
-  fontSize: "text-48",
+  fontSize: "text-48"
+};
+
+export const companyTvDesktopAndTabletNameDefaultThemeClasses: IThemeClasses = {
+  ...companyNameCommonThemeClasses,
   lineHeight: "leading-56",
   textAlign: "text-right"
 };
 
 export const companyMobileNameDefaultThemeClasses: IThemeClasses = {
-  color: "text-white",
-  fontFamily: isIE11() ? "font-anonymousPro" : "font-exan",
-  fontSize: "text-48",
+  ...companyNameCommonThemeClasses,
   lineHeight: "leading-48",
   textAlign: "text-center"
 };
