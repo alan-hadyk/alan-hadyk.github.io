cc3876ea0b606d2bb755e3e05d5e144e587bbfb8
alan-hadyk <alan.hadyk@gmail.com>
2023-03-27 20:00:44 +0200

    Fix merge errors

--- components/molecules/PdfTitle/@types/PdfTitle.ts
+++ components/molecules/PdfTitle/@types/PdfTitle.ts
@@ -1,10 +1,6 @@
 import { TChildren } from "types/props";
 
-<<<<<<<< HEAD:components/molecules/PdfTitle/@types/PdfTitle.ts
 export interface IPdfTitleProps {
-========
-export interface IPdfTemplateProps {
->>>>>>>> master:components/templates/PdfTemplate/@types/PdfTemplate.ts
   children: TChildren;
   sidebar: TChildren;
 }
--- containers/sidebar/SidebarContainer.tsx
+++ containers/sidebar/SidebarContainer.tsx
@@ -5,52 +5,50 @@ import { IconsWithLabelsPosition } from "components/molecules/IconsWithLabels/@t
 import { IconsWithLabels } from "components/molecules/IconsWithLabels/IconsWithLabels";
 import { IconWithLabelSize } from "components/molecules/IconWithLabel/@types/IconWithLabel";
 
-const SidebarContainer: React.FC = () => {
-  return (
-    <>
-      <ImageDynamic
-        imageName="logo"
-        themeClasses={{
-          height: "h-auto",
-          marginBottom: "mb-4"
-        }}
-      />
+const SidebarContainer: React.FC = () => (
+  <>
+    <ImageDynamic
+      imageName="logo"
+      themeClasses={{
+        height: "h-auto",
+        marginBottom: "mb-4"
+      }}
+    />
 
-      <Typography
-        themeClasses={{
-          color: "text-white",
-          fontSize: "text-12",
-          lineHeight: "leading-16"
-        }}
-      >
-        Front-End Architect
-      </Typography>
+    <Typography
+      themeClasses={{
+        color: "text-white",
+        fontSize: "text-12",
+        lineHeight: "leading-16"
+      }}
+    >
+      Front-End Architect
+    </Typography>
 
-      <IconsWithLabels
-        iconsWithLabels={[
-          {
-            href: "https://github.com/alan-hadyk",
-            iconName: IconStaticName.GitHub,
-            isExternal: true,
-            label: "GitHub"
-          },
-          {
-            href: "https://www.linkedin.com/in/alan-hadyk-78738099/",
-            iconName: IconStaticName.LinkedIn,
-            isExternal: true,
-            label: "LinkedIn"
-          }
-        ]}
-        position={IconsWithLabelsPosition.Vertical}
-        size={IconWithLabelSize.Small}
-        themeClasses={{
-          wrapper: {
-            height: "h-auto"
-          }
-        }}
-      />
-    </>
-  );
-};
+    <IconsWithLabels
+      iconsWithLabels={[
+        {
+          href: "https://github.com/alan-hadyk",
+          iconName: IconStaticName.GitHub,
+          isExternal: true,
+          label: "GitHub"
+        },
+        {
+          href: "https://www.linkedin.com/in/alan-hadyk-78738099/",
+          iconName: IconStaticName.LinkedIn,
+          isExternal: true,
+          label: "LinkedIn"
+        }
+      ]}
+      position={IconsWithLabelsPosition.Vertical}
+      size={IconWithLabelSize.Small}
+      themeClasses={{
+        wrapper: {
+          height: "h-auto"
+        }
+      }}
+    />
+  </>
+);
 
 export { SidebarContainer };
