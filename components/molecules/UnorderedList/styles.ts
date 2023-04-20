387aab85ada20421af8f6dd949979ce2447852dd
alan-hadyk <alan.hadyk@gmail.com>
2023-04-20 12:56:30 +0200

    Fix UnorderedList styles

--- components/molecules/UnorderedList/styles.ts
+++ components/molecules/UnorderedList/styles.ts
@@ -4,36 +4,14 @@ import { IThemeClasses, TPseudoClasses } from "types/theme";
 const commonPseudoClasses: TPseudoClasses = [
   "childrenLi:items-start",
   "childrenLi:flex",
-  "childrenLi:mb-16",
   "childrenLi:before:bg-blue100",
   "childrenLi:before:rounded-full",
   "childrenLi:before:content-['']",
-  "childrenLi:before:inline-block",
-  "childrenLi:before:h-8",
-  "childrenLi:before:mr-8",
-  "childrenLi:before:mt-10",
-  "childrenLi:before:min-h-8",
-  "childrenLi:before:min-w-8",
-  "childrenLi:before:w-8"
+  "childrenLi:before:inline-block"
 ];
 
 export const unorderedListDefaultThemeClasses: IThemeClasses = {
-  listStyleType: "list-none",
-  pseudoClasses: [
-    "childrenLi:items-start",
-    "childrenLi:flex",
-    "childrenLi:mb-16",
-    "childrenLi:before:bg-blue100",
-    "childrenLi:before:rounded-full",
-    "childrenLi:before:content-['']",
-    "childrenLi:before:inline-block",
-    "childrenLi:before:h-8",
-    "childrenLi:before:mr-8",
-    "childrenLi:before:mt-10",
-    "childrenLi:before:min-h-8",
-    "childrenLi:before:min-w-8",
-    "childrenLi:before:w-8"
-  ]
+  listStyleType: "list-none"
 };
 export const mapUnorderedListSizeToPseudoClasses: Record<
   UnorderedListSize,
