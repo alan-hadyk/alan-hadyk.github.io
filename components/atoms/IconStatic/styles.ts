bcc1b5507af94ff1d122652e1a0e77a07f3aba68
alan-hadyk <alan.hadyk@gmail.com>
2023-04-21 20:31:43 +0200

    Fix iconStaticDefaultThemeClasses

--- components/atoms/IconStatic/styles.ts
+++ components/atoms/IconStatic/styles.ts
@@ -1,9 +1,7 @@
 import { IIconStaticProps } from "components/atoms/IconStatic/@types/IconStatic";
 
-const iconStaticDefaultThemeClasses: IIconStaticProps["themeClasses"] = {
+export const iconStaticDefaultThemeClasses: IIconStaticProps["themeClasses"] = {
   height: "h-full",
   overflow: "overflow-visible",
   width: "w-auto"
 };
-
-export { iconStaticDefaultThemeClasses };
