308c818b145d19366f8a85d85213b30dd4cc409d
alan-hadyk <alan.hadyk@gmail.com>
2023-04-21 23:04:43 +0200

    Refactor PdfTitle styles

--- components/molecules/PdfTitle/PdfTitle.tsx
+++ components/molecules/PdfTitle/PdfTitle.tsx
@@ -6,27 +6,14 @@ import { Corner } from "components/atoms/Corner/Corner";
 import { Typography } from "components/atoms/Typography/Typography";
 import { LayoutContainer } from "components/layout/LayoutContainer/LayoutContainer";
 import { IPdfTitleProps } from "components/molecules/PdfTitle/@types/PdfTitle";
+import {
+  pdfTitleDefaultThemeClasses,
+  pdfTitleTypographyDefaultThemeClasses
+} from "components/molecules/PdfTitle/styles";
 
 const PdfTitle: React.FC<IPdfTitleProps> = ({ children }) => (
-  <LayoutContainer
-    themeClasses={{
-      paddingBottom: "pb-16",
-      paddingTop: "pt-40",
-      textAlign: "text-center"
-    }}
-  >
-    <Typography
-      themeClasses={{
-        color: "text-blue600",
-        display: "inline-block",
-        fontFamily: "font-exan",
-        fontSize: "text-40",
-        height: "h-44",
-        lineHeight: "leading-44",
-        paddingX: "px-8",
-        position: "relative"
-      }}
-    >
+  <LayoutContainer themeClasses={pdfTitleDefaultThemeClasses}>
+    <Typography themeClasses={pdfTitleTypographyDefaultThemeClasses}>
       <Corner position={CornerPosition.TopLeft} variant={CornerVariant.Dark} />
       <Corner
         position={CornerPosition.BottomRight}
---
+++ components/molecules/PdfTitle/styles.ts
@@ -0,0 +1,18 @@
+import { IThemeClasses } from "types/theme";
+
+export const pdfTitleDefaultThemeClasses: IThemeClasses = {
+  paddingBottom: "pb-16",
+  paddingTop: "pt-40",
+  textAlign: "text-center"
+};
+
+export const pdfTitleTypographyDefaultThemeClasses: IThemeClasses = {
+  color: "text-blue600",
+  display: "inline-block",
+  fontFamily: "font-exan",
+  fontSize: "text-40",
+  height: "h-44",
+  lineHeight: "leading-44",
+  paddingX: "px-8",
+  position: "relative"
+};
