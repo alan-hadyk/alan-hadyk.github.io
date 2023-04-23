2f883e74458fcd13b606843db03a3059545fd2cf
alan-hadyk <alan.hadyk@gmail.com>
2023-04-23 15:09:01 +0200

    Simplify PdfTitle styles

--- components/molecules/PdfTitle/PdfTitle.tsx
+++ components/molecules/PdfTitle/PdfTitle.tsx
@@ -6,22 +6,26 @@ import { Corner } from "components/atoms/Corner/Corner";
 import { Typography } from "components/atoms/Typography/Typography";
 import { LayoutContainer } from "components/layout/LayoutContainer/LayoutContainer";
 import { IPdfTitleProps } from "components/molecules/PdfTitle/@types/PdfTitle";
-import {
-  pdfTitleDefaultThemeClasses,
-  pdfTitleTypographyDefaultThemeClasses
-} from "components/molecules/PdfTitle/styles";
+import { pdfTitleDefaultThemeClasses } from "components/molecules/PdfTitle/styles";
 
-const PdfTitle: React.FC<IPdfTitleProps> = ({ children }) => (
-  <LayoutContainer themeClasses={pdfTitleDefaultThemeClasses}>
-    <Typography themeClasses={pdfTitleTypographyDefaultThemeClasses}>
-      <Corner position={CornerPosition.TopLeft} variant={CornerVariant.Dark} />
-      <Corner
-        position={CornerPosition.BottomRight}
-        variant={CornerVariant.Dark}
-      />
-      {children}
-    </Typography>
-  </LayoutContainer>
-);
+const PdfTitle: React.FC<IPdfTitleProps> = ({ children }) => {
+  const { container, typography } = pdfTitleDefaultThemeClasses;
+
+  return (
+    <LayoutContainer themeClasses={container}>
+      <Typography themeClasses={typography}>
+        <Corner
+          position={CornerPosition.TopLeft}
+          variant={CornerVariant.Dark}
+        />
+        <Corner
+          position={CornerPosition.BottomRight}
+          variant={CornerVariant.Dark}
+        />
+        {children}
+      </Typography>
+    </LayoutContainer>
+  );
+};
 
 export { PdfTitle };
--- components/molecules/PdfTitle/styles.ts
+++ components/molecules/PdfTitle/styles.ts
@@ -1,18 +1,22 @@
 import { IThemeClasses } from "types/theme";
 
-export const pdfTitleDefaultThemeClasses: IThemeClasses = {
-  paddingBottom: "pb-16",
-  paddingTop: "pt-40",
-  textAlign: "text-center"
-};
-
-export const pdfTitleTypographyDefaultThemeClasses: IThemeClasses = {
-  color: "text-blue600",
-  display: "inline-block",
-  fontFamily: "font-exan",
-  fontSize: "text-40",
-  height: "h-44",
-  lineHeight: "leading-44",
-  paddingX: "px-8",
-  position: "relative"
+export const pdfTitleDefaultThemeClasses: Record<
+  "container" | "typography",
+  IThemeClasses
+> = {
+  container: {
+    paddingBottom: "pb-16",
+    paddingTop: "pt-40",
+    textAlign: "text-center"
+  },
+  typography: {
+    color: "text-blue600",
+    display: "inline-block",
+    fontFamily: "font-exan",
+    fontSize: "text-40",
+    height: "h-44",
+    lineHeight: "leading-44",
+    paddingX: "px-8",
+    position: "relative"
+  }
 };
