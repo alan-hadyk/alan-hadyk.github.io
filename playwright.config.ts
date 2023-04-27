b6cad05492ce18e1bb0aa74acebcb4d5f7a90988
alan-hadyk <alan.hadyk@gmail.com>
2023-04-27 17:53:32 +0200

    Update playwright config

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,10 +1,11 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
 import { defineConfig, devices } from "@playwright/test";
 
 /**
  * Read environment variables from file.
  * https://github.com/motdotla/dotenv
  */
-// require('dotenv').config();
+require("dotenv").config();
 
 /**
  * See https://playwright.dev/docs/test-configuration.
