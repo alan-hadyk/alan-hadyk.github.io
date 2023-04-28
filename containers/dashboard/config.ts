09325f3d6f7fae9e13d283d601bc5404c3f19efc
alan-hadyk <alan.hadyk@gmail.com>
2023-04-28 16:37:47 +0200

    Fix unit tests, cause e2e failure

--- containers/dashboard/config.ts
+++ containers/dashboard/config.ts
@@ -1,2 +1,1 @@
-export const catchPhrase =
-  "Innovative and results-driven front-end architect, dedicated to transforming user experience through strategic UI design and development";
+export const catchPhrase = "design and development";
