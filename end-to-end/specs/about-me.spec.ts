26fb3ec8f9a985394002139580c66d2745544f24
alan-hadyk <alan.hadyk@gmail.com>
2023-05-31 20:17:57 +0200

    use video in e2e

--- end-to-end/specs/about-me.spec.ts
+++ end-to-end/specs/about-me.spec.ts
@@ -1,6 +1,8 @@
 import { test, expect } from "@playwright/test";
 import { apiGithubCom } from "../mock/apiGithubCom";
 
+test.use({ video: "on" });
+
 test.beforeEach(async ({ page }) => {
   await page.route(
     "https://api.github.com/repos/alan-hadyk/portfolio/commits",
